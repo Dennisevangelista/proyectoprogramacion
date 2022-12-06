a056e4052cd0d80df37513bed31b1c36da0017ba
Dennisevangelista <88729320+Dennisevangelista@users.noreply.github.com>
2022-12-06 12:12:16 -0500

    dennis

--- nosotros.php
+++ nosotros.php
@@ -1,20 +1,99 @@
 <?php
-  $page_title = 'Home Page';
+  $page_title = 'Nosotros';
+  
   require_once('includes/load.php');
-  if (!$session->isUserLoggedIn(true)) { redirect('index.php', false);}
+  
 ?>
 <?php include_once('layouts/header.php'); ?>
-<div class="row">
-  <div class="col-md-12">
-    <?php echo display_msg($msg); ?>
-  </div>
- <div class="col-md-12">
-    <div class="panel">
-      <div class="jumbotron text-center">
-         <h1>Esta es su nueva página de contacto</h1>
-     
+<meta charset="UTF-8">
+<header id="header">
+<div class="navbar-collapse collapse">
+          <div class="menu">
+            <ul class="nav nav-tabs" role="tablist">
+              <li role="presentation"><a href="index.html" class="active">Inicio</a></li>
+              <li role="presentation"><a href="about.html">Establecimientos</a></li>
+              <li role="presentation"><a href="services.html">Equipo</a></li>
+              <li role="presentation"><a href="nosotros.php">Nosotros</a></li>
+              <li role="presentation"><a href="contacto.php">Contacto</a></li>
+            </ul>
+          </div>
+        </div>
+  </header>
+   <!-- #fin header -->
+   <section id="works" class="section gray">
+    <div class="container">
+      <section class="b1nosotros">
+    <div class="b1nosotros_content">
+      <figure class="b1nosotros_portada" style="background-image:url('assets/img/dummies/team1.jpg');"></figure>
+      <div class="b1nosotros_info">
+        <div class="b1nosotros_cuadro">
+          <div class="b1nosotros_boxwrap">
+            <div class="b1nosotros_title">
+              <h1>Desayuna con la panadera</h1>
+            </div>
+            <div class="b1nosotros_text">
+              <p style="text-align:justify">La Panadera es una empresa familiar que nació a principios de los años 80 en el pueblo de Azagra (Navarra), con el fin de servir pan tanto a particulares como a tiendas, bares, restaurantes y supermercados.
+
+La Panadera ha sido siempre una empresa preocupada por satisfacer las necesidades de sus clientes, ofreciendo siempre la máxima calidad tanto en sus productos como en sus servicios.</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
- </div>
-</div>
+      </section>
+     <section class="b2nosotros">
+    <div class="b2nosotros_content ww_center">
+      <div class="b2nosotros_items">
+        <div class="b2nosotros_box">
+          <div class="b2nosotros_subtitle">
+            <h2>Misión</h2>
+          </div>
+          <div class="b2nosotros_text">
+            <p style="text-align:justify">Su misión es ser la empresa líder en el negocio de la panadería y repostería, superando las expectativas de los clientes, a través de un equipo comprometido.</p>
+          </div>
+        </div>
+        <div class="b2nosotros_box">
+          <div class="b2nosotros_subtitle">
+            <h2>Visión</h2>
+          </div>
+          <div class="b2nosotros_text">
+            <p style="margin-left:0cm; margin-right:0cm; text-align:justify">Ofrecemos un servicio rápido y seguro para el transporte de nuestros productos al lugar que el cliente desee. Además de disponer de un servicio a domicilio, también comercializamos nuestros productos por el resto de provincias.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+   
+  <footer>
+    <div class="verybottom">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="aligncenter">
+              <ul class="social-network social-circle">
+                <li><a href="#" class="icoRss" title="Rss"><i class="fa fa-rss"></i></a></li>
+                <li><a href="#" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+                <li><a href="#" class="icoTwitter" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+                <li><a href="#" class="icoGoogle" title="Google +"><i class="fa fa-google-plus"></i></a></li>
+                <li><a href="#" class="icoLinkedin" title="Linkedin"><i class="fa fa-linkedin"></i></a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-12">
+            <div class="aligncenter">
+              <p>
+                &copy;Grupo 2 utp - All right reserved
+              </p>
+              <div class="credits">
+             
+                <a href="https://www.youtube.com">Grupo 2 </a> by <a href="https://www.youtube.com">UTP</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </footer>
 <?php include_once('layouts/footer.php'); ?>
